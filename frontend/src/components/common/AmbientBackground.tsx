bf1be9bd624c3f397f8b481b716f21a1fb029e2c
oussam hrizi <oussamahrizi2@gmail.com>
2026-07-17 00:58:16 +0100

    remove frontend directory and all its contents

--- frontend/src/components/common/AmbientBackground.tsx
+++
@@ -1,9 +0,0 @@
-export function AmbientBackground() {
-  return (
-    <div className="ambient-bg" aria-hidden="true">
-      <div className="ambient-blob blob-teal" />
-      <div className="ambient-blob blob-violet" />
-      <div className="grid-overlay" />
-    </div>
-  );
-}
